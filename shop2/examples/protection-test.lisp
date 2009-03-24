71565b9fead5e7bbcb7177d0832fafb213e27fa9
Robert P. Goldman <rpgoldman@sift.net>
2009-03-24 20:29:32 +0000

    With John's help, fixed a bug in protections test that came when the semantics of group fixtures changed.

--- shop2/examples/protection-test.lisp
+++ shop2/examples/protection-test.lisp
@@ -10,7 +10,7 @@
 ;;;
 ;;;    This file is intended to supply a number of unit tests to
 ;;;    determine whether or not protections are working.  Propose the
-;;;    following test structure: 
+;;;    following test structure:
 ;;;    1.  Check to make sure that an action can correctly be applied
 ;;;    to a state (background test).  This state will violate a
 ;;;    protection added by the action in 2, below.  Modified to make
@@ -41,187 +41,188 @@
   (defdomain protection-test-domain
     (
      (:operator (!positive-op)
-		()			;no preconditions
-		()
-		((prop)))
+                ()                      ;no preconditions
+                ()
+                ((prop)))
      (:operator (!negative-op)
-		()			;no preconditions
-		((prop))
-		())
+                ()                      ;no preconditions
+                ((prop))
+                ())
      (:operator (!add-protect)
-		()
-		()
-		((:protection (prop))))
+                ()
+                ()
+                ((:protection (prop))))
      (:operator (!remove-protect)
-		()
-		((:protection (prop)))
-		())
+                ()
+                ((:protection (prop)))
+                ())
      (:operator (!add-neg-protect)
-		()
-		()
-		((:protection (not (prop)))))
+                ()
+                ()
+                ((:protection (not (prop)))))
      (:operator (!remove-neg-protect)
-		()
-		((:protection (not (prop))))
-		()))))
+                ()
+                ((:protection (not (prop))))
+                ()))))
 
 (def-fixtures protections-domain ()
    (setup
-    (init-protections-domain))
+    (init-protections-domain)))
+
+(def-fixtures empty-state ()
    (s (shop::make-initial-state shop::*domain*
-				:list nil)))
+                                :list nil)))
 
 (defmacro apply-op (opname &key protections)
   `(locally
     (declare (special shop::*domain* s))
     (apply-operator shop::*domain* s
-		    '( ,opname )
-		    (shop::operator shop::*domain* ',opname)
-		    ,protections 0 nil)))
+                    '( ,opname )
+                    (shop::operator shop::*domain* ',opname)
+                    ,protections 0 nil)))
 
 (nst:def-value-check (:failed () (retval &rest args))
     `(if (eq retval 'fail)
-	 (sift.nst::make-check-result)
-	 (sift.nst:emit-failure)))
+         (sift.nst::make-check-result)
+         (sift.nst:emit-failure)))
 
 (nst:def-value-check (:unfailed () (retval &rest args))
     `(if (eq retval 'fail)
-	 (sift.nst:emit-failure)
-	 (sift.nst::make-check-result)))
+         (sift.nst:emit-failure)
+         (sift.nst::make-check-result)))
 
-    
+
 
 (def-test-group protection-test (protections-domain)
-   ;; item number one
-   (def-check can-apply-pos-op
-       (:unfailed) (apply-op !positive-op))
-   (def-check can-apply-neg-op
-       (:unfailed) (apply-op !negative-op))
-   ;; item number two
-   (def-check can-apply-add-protect
-       (:unfailed)
-	(progn (apply-op !positive-op)
-	       (apply-op !add-protect)))
-   (def-check can-apply-remove-protect
-       (:unfailed) (apply-op !remove-protect))
-   (def-check can-apply-add-neg-protect
-       (:unfailed) (apply-op !add-neg-protect))
-   (def-check can-apply-remove-neg-protect
-       (:unfailed) (apply-op !remove-neg-protect))
+  ;; item number one
+  (def-check  (can-apply-pos-op :fixtures (empty-state))
+      (:unfailed) (apply-op !positive-op))
+  (def-check  (can-apply-neg-op :fixtures (empty-state))
+      (:unfailed) (apply-op !negative-op))
+  ;; item number two
+  (def-check  (can-apply-add-protect :fixtures (empty-state))
+      (:unfailed)
+    (progn (apply-op !positive-op)
+           (apply-op !add-protect)))
+  (def-check  (can-apply-remove-protect :fixtures (empty-state))
+      (:unfailed) (apply-op !remove-protect))
+  (def-check  (can-apply-add-neg-protect :fixtures (empty-state))
+      (:unfailed) (apply-op !add-neg-protect))
+  (def-check  (can-apply-remove-neg-protect :fixtures (empty-state))
+      (:unfailed) (apply-op !remove-neg-protect))
 
-   ;; check protections before adding
-   (def-check protection-init-check
-       (:err) (apply-op !add-protect))
-   (def-check protection-init-check-neg
-       (:err)
-	(progn
-	  (apply-op !positive-op)
-	  (apply-op !add-neg-protect)))
+  ;; check protections before adding
+  (def-check  (protection-init-check :fixtures (empty-state))
+      (:err) (apply-op !add-protect))
+  (def-check  (protection-init-check-neg :fixtures (empty-state))
+      (:err)
+    (progn
+      (apply-op !positive-op)
+      (apply-op !add-neg-protect)))
 
-   ;; item number three
-   (def-check protection-effective
-       (:failed)
-     (progn
-       (apply-op !positive-op)
-       (multiple-value-bind (op tag protections)
-	   (apply-op !add-protect)
-	 (declare (ignore op tag) (special shop::*domain* s))
-	 (apply-operator shop::*domain* s '(!negative-op)
-			 (shop::operator shop::*domain* '!negative-op)
-			 protections 0 nil))))
-   (def-check neg-protection-effective
-       (:failed)
-	(multiple-value-bind (op tag protections)
-	    (apply-op !add-neg-protect)
-	  (declare (ignore op tag) (special shop::*domain* s))
-	  (apply-operator shop::*domain* s '(!positive-op)
-			  (shop::operator shop::*domain* '!negative-op)
-			  protections 0 nil)))
+  ;; item number three
+  (def-check  (protection-effective :fixtures (empty-state))
+      (:failed)
+    (progn
+      (apply-op !positive-op)
+      (multiple-value-bind (op tag protections)
+          (apply-op !add-protect)
+        (declare (ignore op tag) (special shop::*domain* s))
+        (apply-operator shop::*domain* s '(!negative-op)
+                        (shop::operator shop::*domain* '!negative-op)
+                        protections 0 nil))))
+  (def-check  (neg-protection-effective :fixtures (empty-state))
+      (:failed)
+    (multiple-value-bind (op tag protections)
+        (apply-op !add-neg-protect)
+      (declare (ignore op tag) (special shop::*domain* s))
+      (apply-operator shop::*domain* s '(!positive-op)
+                      (shop::operator shop::*domain* '!negative-op)
+                      protections 0 nil)))
 
-   ;; items number four and five
-   (def-check protection-add-and-remove
-       (:unfailed)
-     (progn
-       (apply-op !positive-op)
-       (multiple-value-bind (op tag protections)
-	   (apply-op !add-protect)
-	 (declare (ignore op tag) (special shop::*domain* s))
-	 (multiple-value-bind (op tag protections)
-	     (apply-op !remove-protect :protections protections)
-	   (declare (ignore op tag))
-	   (apply-op !negative-op :protections protections)))))
-   (def-check neg-protection-add-and-remove
-       (:unfailed)
-     (progn
-       (multiple-value-bind (op tag protections)
-	   (apply-op !add-neg-protect)
-	 (declare (ignore op tag) (special shop::*domain* s))
-	 (multiple-value-bind (op tag protections)
-	     (apply-op !remove-neg-protect :protections protections)
-	   (declare (ignore op tag))
-	   (apply-op !positive-op :protections protections)))))
+  ;; items number four and five
+  (def-check  (protection-add-and-remove :fixtures (empty-state))
+      (:unfailed)
+    (progn
+      (apply-op !positive-op)
+      (multiple-value-bind (op tag protections)
+          (apply-op !add-protect)
+        (declare (ignore op tag) (special shop::*domain* s))
+        (multiple-value-bind (op tag protections)
+            (apply-op !remove-protect :protections protections)
+          (declare (ignore op tag))
+          (apply-op !negative-op :protections protections)))))
+  (def-check  (neg-protection-add-and-remove :fixtures (empty-state))
+      (:unfailed)
+    (progn
+      (multiple-value-bind (op tag protections)
+          (apply-op !add-neg-protect)
+        (declare (ignore op tag) (special shop::*domain* s))
+        (multiple-value-bind (op tag protections)
+            (apply-op !remove-neg-protect :protections protections)
+          (declare (ignore op tag))
+          (apply-op !positive-op :protections protections)))))
 
-   ;; item number six --- protection arithmetic
-   (def-check protection-arithmetic-positive
-       (:failed)
-     (progn
-       (apply-op !positive-op)
-       (multiple-value-bind (op tag protections)
-	   (apply-op !add-protect)
-	 (declare (ignore op tag) (special shop::*domain* s))
-	 (multiple-value-bind (op tag protections)
-	     (apply-op !add-protect :protections protections)
-	   (declare (ignore op tag))
-	   (multiple-value-bind (op tag protections)
-	       (apply-op !remove-protect :protections protections)
-	     (declare (ignore op tag))
-	     (apply-op !negative-op :protections protections))))))
-   (def-check protection-arithmetic-positive-2
-       (:unfailed)
-     (progn
-       (apply-op !positive-op)
-       (multiple-value-bind (op tag protections)
-	   (apply-op !add-protect)
-	 (declare (ignore op tag) (special shop::*domain* s))
-	 (multiple-value-bind (op tag protections)
-	     (apply-op !add-protect :protections protections)
-	   (declare (ignore op tag))
-	   (multiple-value-bind (op tag protections)
-	     (apply-op !remove-protect :protections protections)
-	     (declare (ignore op tag))
-	     (multiple-value-bind (op tag protections)
-		 (apply-op !remove-protect :protections protections)
-	       (declare (ignore op tag))
-	   (apply-op !negative-op :protections protections)))))))
-   (def-check protection-arithmentic-neg-1
-       (:failed)
-       (multiple-value-bind (op tag protections)
-	   (apply-op !add-neg-protect)
-	 (declare (ignore op tag) (special shop::*domain* s))
-	 (multiple-value-bind (op tag protections)
-	   (apply-op !add-neg-protect :protections protections)
-	   (declare (ignore op tag))
-	   (multiple-value-bind (op tag protections)
-	       (apply-op !remove-neg-protect :protections protections)
-	     (declare (ignore op tag))
-	     (apply-op !positive-op :protections protections)))))
-   (def-check protection-arithmentic-neg-2
-       (:unfailed)
-     (multiple-value-bind (op tag protections)
-	 (apply-op !add-neg-protect)
-       (declare (ignore op tag) (special shop::*domain* s))
-       (multiple-value-bind (op tag protections)
-	   (apply-op !add-neg-protect :protections protections)
-	 (declare (ignore op tag))
-	 (multiple-value-bind (op tag protections)
-	     (apply-op !remove-neg-protect :protections protections)
-	   (declare (ignore op tag))
-	   (multiple-value-bind (op tag protections)
-	       (apply-op !remove-neg-protect :protections protections)
-	     (declare (ignore op tag))
-	     (apply-op !positive-op :protections protections))))))
-   )
+  ;; item number six --- protection arithmetic
+  (def-check  (protection-arithmetic-positive :fixtures (empty-state))
+      (:failed)
+    (progn
+      (apply-op !positive-op)
+      (multiple-value-bind (op tag protections)
+          (apply-op !add-protect)
+        (declare (ignore op tag) (special shop::*domain* s))
+        (multiple-value-bind (op tag protections)
+            (apply-op !add-protect :protections protections)
+          (declare (ignore op tag))
+          (multiple-value-bind (op tag protections)
+              (apply-op !remove-protect :protections protections)
+            (declare (ignore op tag))
+            (apply-op !negative-op :protections protections))))))
+  (def-check  (protection-arithmetic-positive-2 :fixtures (empty-state))
+      (:unfailed)
+    (progn
+      (apply-op !positive-op)
+      (multiple-value-bind (op tag protections)
+          (apply-op !add-protect)
+        (declare (ignore op tag) (special shop::*domain* s))
+        (multiple-value-bind (op tag protections)
+            (apply-op !add-protect :protections protections)
+          (declare (ignore op tag))
+          (multiple-value-bind (op tag protections)
+              (apply-op !remove-protect :protections protections)
+            (declare (ignore op tag))
+            (multiple-value-bind (op tag protections)
+                (apply-op !remove-protect :protections protections)
+              (declare (ignore op tag))
+              (apply-op !negative-op :protections protections)))))))
+  (def-check  (protection-arithmentic-neg-1 :fixtures (empty-state))
+      (:failed)
+    (multiple-value-bind (op tag protections)
+        (apply-op !add-neg-protect)
+      (declare (ignore op tag) (special shop::*domain* s))
+      (multiple-value-bind (op tag protections)
+          (apply-op !add-neg-protect :protections protections)
+        (declare (ignore op tag))
+        (multiple-value-bind (op tag protections)
+            (apply-op !remove-neg-protect :protections protections)
+          (declare (ignore op tag))
+          (apply-op !positive-op :protections protections)))))
+  (def-check  (protection-arithmentic-neg-2 :fixtures (empty-state))
+      (:unfailed)
+    (multiple-value-bind (op tag protections)
+        (apply-op !add-neg-protect)
+      (declare (ignore op tag) (special shop::*domain* s))
+      (multiple-value-bind (op tag protections)
+          (apply-op !add-neg-protect :protections protections)
+        (declare (ignore op tag))
+        (multiple-value-bind (op tag protections)
+            (apply-op !remove-neg-protect :protections protections)
+          (declare (ignore op tag))
+          (multiple-value-bind (op tag protections)
+              (apply-op !remove-neg-protect :protections protections)
+            (declare (ignore op tag))
+            (apply-op !positive-op :protections protections))))))
+  )
 
-   
-     
-		
+
+
