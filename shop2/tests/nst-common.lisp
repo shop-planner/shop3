2a9b5d76ba6af82c831bf7831be68c5bb21f8535
Robert P. Goldman <rpgoldman@sift.net>
2009-04-01 02:50:48 +0000

    Modified to keep find-plans from blatting so much verbosity onto the console during tests.

--- shop2/tests/nst-common.lisp
+++ shop2/tests/nst-common.lisp
@@ -7,8 +7,8 @@
     `(cond
        ((null plan-list) (nst::make-check-result))
        (t (nst:emit-failure
-	   :format "~@<Expected no plans but found ~d:~{~_ ~s~}~:>"
-	   :args (list (length plan-list) plan-list)))))
+           :format "~@<Expected no plans but found ~d:~{~_ ~s~}~:>"
+           :args (list (length plan-list) plan-list)))))
 
 (nst:def-value-check (:found-plan () (plan-list runtime))
     `(declare (ignorable runtime))
@@ -20,27 +20,30 @@
   "The SHOP2 plans come with the operators interspersed with their
 costs.  This function just throws away the costs."
   (loop with planlist = plan-and-costs
-	while planlist
-	for (operator cost . rest) = planlist
-	do (assert (numberp cost))
-	collect operator
-	do (setf planlist rest)))
+        while planlist
+        for (operator cost . rest) = planlist
+        do (assert (numberp cost))
+        collect operator
+        do (setf planlist rest)))
 
 (nst:def-value-check (:correct-plan (target-plan)
-				    (plan-list runtime))
+                                    (plan-list runtime))
   `(declare (ignorable runtime))
   `(let ((plan (remove-plan-costs (first plan-list))))
      (cond
       ((equal ',target-plan plan)  (nst::make-check-result))
       (t  (nst:emit-failure :format "Unexpected plan ~s"
-			    :args (list plan))))))
+                            :args (list plan))))))
+
+(defun plan-quietly (problem &rest args)
+  (apply #'find-plans problem :which :first :verbose 0 args))
 
 (nst:def-check-alias (:plan-problem criterion)
-  `(:apply find-plans ,criterion))
+  `(:apply plan-quietly ,criterion))
 
 (nst:def-check-alias (:primary-result-plan target-plan)
     `(:plan-problem (:all :found-plan
-			  (:correct-plan ,target-plan))))
+                          (:correct-plan ,target-plan))))
 
 (nst:def-check-alias (:no-result-plans)
     `(:plan-problem :no-plan))
