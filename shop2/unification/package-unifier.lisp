705a3e2a0e4131408c79575a5b99563b966884f4
Robert P. Goldman <rpgoldman@sift.net>
2009-10-06 22:51:37 +0000

    Turned variablep from a macro into a function.  If it's a macro, then we can't use it in deftype, which means we have no good way of doing typep.
    Also defined a shop-variable type in terms of variablep, and exported it from the shop2.unifier package.

--- shop2/unification/package-unifier.lisp
+++ shop2/unification/package-unifier.lisp
@@ -1,19 +1,19 @@
 ;;; -*- Mode: common-lisp; package: common-lisp-user; -*-
 ;;;
 ;;; Version: MPL 1.1/GPL 2.0/LGPL 2.1
-;;; 
+;;;
 ;;; The contents of this file are subject to the Mozilla Public License
 ;;; Version 1.1 (the "License"); you may not use this file except in
 ;;; compliance with the License. You may obtain a copy of the License at
 ;;; http://www.mozilla.org/MPL/
-;;; 
+;;;
 ;;; Software distributed under the License is distributed on an "AS IS"
 ;;; basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 ;;; License for the specific language governing rights and limitations under
 ;;; the License.
-;;; 
-;;; The Original Code is SHOP2.  
-;;; 
+;;;
+;;; The Original Code is SHOP2.
+;;;
 ;;; The Initial Developer of the Original Code is the University of
 ;;; Maryland. Portions created by the Initial Developer are Copyright (C)
 ;;; 2002,2003 the Initial Developer. All Rights Reserved.
@@ -22,8 +22,8 @@
 ;;; Portions created by Drs. Goldman and Maraist are Copyright (C)
 ;;; 2004-2007 SIFT, LLC.  These additions and modifications are also
 ;;; available under the MPL/GPL/LGPL licensing terms.
-;;; 
-;;; 
+;;;
+;;;
 ;;; Alternatively, the contents of this file may be used under the terms of
 ;;; either of the GNU General Public License Version 2 or later (the "GPL"),
 ;;; or the GNU Lesser General Public License Version 2.1 or later (the
@@ -39,16 +39,16 @@
 ;;; ----------------------------------------------------------------------
 
 ;;; Smart Information Flow Technologies Copyright 2006-2007 Unpublished work
-;;; 
+;;;
 ;;; GOVERNMENT PURPOSE RIGHTS
-;;; 
-;;; Contract No.         FA8650-06-C-7606, 
+;;;
+;;; Contract No.         FA8650-06-C-7606,
 ;;; Contractor Name      Smart Information Flow Technologies, LLC
 ;;;                      d/b/a SIFT, LLC
 ;;; Contractor Address   211 N 1st Street, Suite 300
 ;;;                      Minneapolis, MN 55401
 ;;; Expiration Date      5/2/2011
-;;; 
+;;;
 ;;; The Government's rights to use, modify, reproduce, release,
 ;;; perform, display, or disclose this software are restricted by
 ;;; paragraph (b)(2) of the Rights in Noncommercial Computer Software
@@ -63,30 +63,32 @@
     (:nicknames :shopunif)
     (:use :common-lisp)
     (:export #:trace-print
-	     #:*shop-trace* #:*shop-trace-stream* #:*trace-query*
+             #:*shop-trace* #:*shop-trace-stream* #:*trace-query*
 
-	     ;; the unifier interface
-	     #:unify-fail #:unify-p
-	     #:unify #:standardize #:fail
-	     #:apply-substitution #:compose-substitutions
-	     #:fix-uninterned-bindings #:binding-list-value
-	     #:make-binding-list #:make-binding
-	     #:binding-var #:binding-val
-	     #:variablep #:groundp
-	     #:set-variable-property
-	     #:variable-gensym
-	     
-	     ;; [mpelican:20090520.2111CDT] This is a bit of a hack, but set-variable-property
-	     ;; also sets primitive property which is only of interest to planner.  
-	     ;; primitivep which checks the primitive property lives in the shop2 package.
-	     ;; So, we export this symbol for the planner's benefit.
-	     #:primitive
-	     
-	     ;; utilities used internally by shop
-	     #:shop-union
-	     #:get-alist
+             ;; the unifier interface
+             #:unify-fail #:unify-p
+             #:unify #:standardize #:fail
+             #:apply-substitution #:compose-substitutions
+             #:fix-uninterned-bindings #:binding-list-value
+             #:make-binding-list #:make-binding
+             #:binding-var #:binding-val
+             #:variablep #:groundp
+             ;; this is defined as a type, so you can use typep with it. [2009/10/06:rpg]
+             #:shop-variable
+             #:set-variable-property
+             #:variable-gensym
 
-	     ;; hook for tracing -- later exported *through* the shop2 package
-	     #:trace-query-hook
-	     
-	     ))
+             ;; [mpelican:20090520.2111CDT] This is a bit of a hack, but set-variable-property
+             ;; also sets primitive property which is only of interest to planner.
+             ;; primitivep which checks the primitive property lives in the shop2 package.
+             ;; So, we export this symbol for the planner's benefit.
+             #:primitive
+
+             ;; utilities used internally by shop
+             #:shop-union
+             #:get-alist
+
+             ;; hook for tracing -- later exported *through* the shop2 package
+             #:trace-query-hook
+
+             ))
--- shop2/unification/unify.lisp
+++ shop2/unification/unify.lisp
@@ -1,19 +1,19 @@
 ;;; -*- Mode: common-lisp; package: shop2.unifier; -*-
 ;;;
 ;;; Version: MPL 1.1/GPL 2.0/LGPL 2.1
-;;; 
+;;;
 ;;; The contents of this file are subject to the Mozilla Public License
 ;;; Version 1.1 (the "License"); you may not use this file except in
 ;;; compliance with the License. You may obtain a copy of the License at
 ;;; http://www.mozilla.org/MPL/
-;;; 
+;;;
 ;;; Software distributed under the License is distributed on an "AS IS"
 ;;; basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 ;;; License for the specific language governing rights and limitations under
 ;;; the License.
-;;; 
-;;; The Original Code is SHOP2.  
-;;; 
+;;;
+;;; The Original Code is SHOP2.
+;;;
 ;;; The Initial Developer of the Original Code is the University of
 ;;; Maryland. Portions created by the Initial Developer are Copyright (C)
 ;;; 2002,2003 the Initial Developer. All Rights Reserved.
@@ -22,8 +22,8 @@
 ;;; Portions created by Drs. Goldman and Maraist are Copyright (C)
 ;;; 2004-2007 SIFT, LLC.  These additions and modifications are also
 ;;; available under the MPL/GPL/LGPL licensing terms.
-;;; 
-;;; 
+;;;
+;;;
 ;;; Alternatively, the contents of this file may be used under the terms of
 ;;; either of the GNU General Public License Version 2 or later (the "GPL"),
 ;;; or the GNU Lesser General Public License Version 2.1 or later (the
@@ -39,16 +39,16 @@
 ;;; ----------------------------------------------------------------------
 
 ;;; Smart Information Flow Technologies Copyright 2006-2007 Unpublished work
-;;; 
+;;;
 ;;; GOVERNMENT PURPOSE RIGHTS
-;;; 
-;;; Contract No.         FA8650-06-C-7606, 
+;;;
+;;; Contract No.         FA8650-06-C-7606,
 ;;; Contractor Name      Smart Information Flow Technologies, LLC
 ;;;                      d/b/a SIFT, LLC
 ;;; Contractor Address   211 N 1st Street, Suite 300
 ;;;                      Minneapolis, MN 55401
 ;;; Expiration Date      5/2/2011
-;;; 
+;;;
 ;;; The Government's rights to use, modify, reproduce, release,
 ;;; perform, display, or disclose this software are restricted by
 ;;; paragraph (b)(2) of the Rights in Noncommercial Computer Software
@@ -60,19 +60,17 @@
 
 (in-package :shop2.unifier)
 
-#|
-(defstruct constraint
-  "Variables can be bound to constraints, which will be handled specially in 
-unification and substitution."
-  op
-  rhs)
-|#
+;;;(defstruct constraint
+;;;  "Variables can be bound to constraints, which will be handled specially in
+;;;unification and substitution."
+;;;  op
+;;;  rhs)
 
 ;;;(defstruct constraint-set
 ;;;  constraint-list)
 
 ;;;(defstruct attrib-variable
-;;;  name					; should always be a variable symbol, at least for now... [2005/11/06:rpg]
+;;;  name                                       ; should always be a variable symbol, at least for now... [2005/11/06:rpg]
 ;;;  constraints)
 
 ;;;(defmethod print-object ((av attrib-variable) str)
@@ -80,37 +78,37 @@ unification and substitution."
 ;;;  (when (attrib-variable-constraints av)
 ;;;    (format str "~{ ~A~}" (attrib-variable-constraints av)))
 ;;;  (format str ">"))
-    
+
 
 ;;;(defun prim-variable-p (x)
 ;;;  (and (symbolp x) (get x 'variable)))
 
 ;;; VARIABLEP returns T if X is a symbol whose name begins with "?"
-; The code below is faster than checking for the ? each time, but
-;  assumes that all variables, have been preprocessed.
-;;; NOTE: I am not at all sure I know why this needs to be a macro... [2005/11/06:rpg]
-(defmacro variablep (x)
-  `(and (symbolp ,x) (get ,x 'variable))
-;;;  `(or (typep ,x 'attrib-variable)
-;;;       (prim-variable-p ,x))
-  )
+;;; The code below is faster than checking for the ? each time, but
+;;;  assumes that all variables, have been preprocessed.
+(defun variablep (x)
+  (and (symbolp x) (get x 'variable)))
 (defmacro variable-p (x)
   "I can't remember the no-hyphen syntax for this call."
   `(variablep ,x))
+
+(deftype shop-variable ()
+    `(satisfies variablep))
+
 ;;; OLD def...
 ; If for some reason (e.g., debugging) you want to work with
 ;  non-preprocessed variables, use the following instead:
 ;(defun variablep (sym) (and (symbolp sym) (equal (elt (symbol-name sym) 0) #\?)))
 
 (defun groundp (literal)
-  "Is LITERAL a ground literal?  Traverse the tree looking for a 
+  "Is LITERAL a ground literal?  Traverse the tree looking for a
 variable."
   (if (atom literal)
       (not (variablep literal))
     (every #'groundp literal)))
 
 (defun variable-name (x) x)
-  
+
 ;;; Lisp has a built-in UNION function, but we need something that returns
 ;;; the same results regardless of what platform SHOP is running on.
 (defun shop-union (s1 s2 &key (test #'eql))
@@ -119,11 +117,11 @@ variable."
 
 ;;;(defun variable-name (x)
 ;;;  (cond ((prim-variable-p x) x)
-;;;	((typep x 'attrib-variable) (attrib-variable-name x))))
+;;;     ((typep x 'attrib-variable) (attrib-variable-name x))))
 
 (defstruct (binding (:constructor make-binding (var val
-						    ; &optional constraints
-						    )))
+                                                    ; &optional constraints
+                                                    )))
   var
   val
   ;; constraints
@@ -135,15 +133,15 @@ variable."
 (defun binding-list-value (var binding-list &optional (if-not-found :error))
   (let ((binding (find-binding var binding-list)))
     (cond (binding (binding-val binding))
-	  ((eq if-not-found :error)
-	   (error "Unable to find binding for ~A in ~S" var binding-list))
-	  (t if-not-found))))
+          ((eq if-not-found :error)
+           (error "Unable to find binding for ~A in ~S" var binding-list))
+          (t if-not-found))))
 
 
 (defun make-binding-list (variables bindings)
   (loop for var in variables
-	as val in bindings
-	collect (make-binding var val)))
+        as val in bindings
+        collect (make-binding var val)))
 
 ;;; APPLY-SUBSTITUTION searches through TARGET, replacing each variable
 ;;; symbol with the corresponding value (if any) in A-LIST  (Dana Nau)
@@ -199,32 +197,32 @@ variable."
     (setf (binding-val pair) (apply-substitution (binding-val pair) sub2))
 ;;;    (when (binding-constraints pair)
 ;;;      (setf (binding-constraints pair)
-;;;	    (apply-substitution (binding-constraints pair) sub2)))
+;;;         (apply-substitution (binding-constraints pair) sub2)))
 ;;;    (when (variable-p (binding-val pair))
 ;;;      ;; we may need to compose constraints...
 ;;;      (let ((prev-bind
-;;;	     (find (binding-val pair)
-;;;		   sub2 :key #'binding-var)))
-;;;	(when (and prev-bind (binding-constraints prev-bind))
-;;;	  (setf (binding-constraints pair)
-;;;		(nconc (binding-constraints pair) (binding-constraints prev-bind))))))
+;;;          (find (binding-val pair)
+;;;                sub2 :key #'binding-var)))
+;;;     (when (and prev-bind (binding-constraints prev-bind))
+;;;       (setf (binding-constraints pair)
+;;;             (nconc (binding-constraints pair) (binding-constraints prev-bind))))))
 ;;;    (when (groundp (binding-val pair))
 ;;;      (when (binding-constraints pair)
-;;;	(multiple-value-bind (success failed-constraint)
-;;;	    (check-constraints pair)
-;;;	  (unless success
-;;;	    (error (make-condition 'constraint-failure
-;;;				   :constraint failed-constraint))))))
+;;;     (multiple-value-bind (success failed-constraint)
+;;;         (check-constraints pair)
+;;;       (unless success
+;;;         (error (make-condition 'constraint-failure
+;;;                                :constraint failed-constraint))))))
     )
   (dolist (pair sub2)
     (push pair sub1))
 ;;;    (let ((prev-bind (find pair sub1 :key #'(lambda (x) (binding-var x)))))
 ;;;      (cond (prev-bind
-;;;	     ;; if there are constraints on sub2, we need to compoose them together with those in sub1
-;;;	     (setf (binding-constraints prev-bind)
-;;;		(nconc (binding-constraints prev-bind) (binding-constraints pair))))
-;;;	    (t
-;;;	     (push pair sub1)))))
+;;;          ;; if there are constraints on sub2, we need to compoose them together with those in sub1
+;;;          (setf (binding-constraints prev-bind)
+;;;             (nconc (binding-constraints prev-bind) (binding-constraints pair))))
+;;;         (t
+;;;          (push pair sub1)))))
   sub1)
 
 (defun check-constraints (binding)
@@ -239,22 +237,22 @@ listing a failed constraint."
 ;;;    (let ((var (binding-var bind)
 ;;;    (when (typep var 'attrib-variable)
 ;;;      (when (attrib-variable-constraints var)
-;;;	;; at this point, I believe that the binding-list should be
-;;;	;; standardized... [2005/11/06:rpg]
-;;;	(let ((val (apply-substitution e
+;;;     ;; at this point, I believe that the binding-list should be
+;;;     ;; standardized... [2005/11/06:rpg]
+;;;     (let ((val (apply-substitution e
   t)
 
 (defmacro unify-fail (e1 e2)
-  "It's painful \(and bug-inducing\) to have to remember to compare the 
+  "It's painful \(and bug-inducing\) to have to remember to compare the
 result of unify with 'fail."
   `(eq (unify ,e1 ,e2) 'shop2.unifier::fail))
 
 (defmacro unify-p (e1 e2)
-  "It's painful \(and bug-inducing\) to have to remember to compare the 
-result of unify with 'fail.  This checks to see whether E1 and E2 
+  "It's painful \(and bug-inducing\) to have to remember to compare the
+result of unify with 'fail.  This checks to see whether E1 and E2
 unify, and returns T or NIL, accordingly."
   `(not (unify-fail ,e1 ,e2)))
-  
+
 ;;; UNIFY is based on the procedure in Nilsson's 1980 book, but modified
 ;;; (Dana Nau)
 ;;; Added handler to trap cases where we unify variables, but fail to
@@ -266,41 +264,41 @@ if they do, or FAIL \(*NOT* nil\) if they don't."
         ((atom e2) (unify1 e2 e1))
         (t (let ((hsub (unify (car e1) (car e2))))
              (if (eql hsub 'fail)
-		 'fail
-		 (let* ((tail1 (apply-substitution (cdr e1) hsub))
-			(tail2 (apply-substitution (cdr e2) hsub))
-			(tsub (unify tail1 tail2)))
-		   (if (eql tsub 'fail)
-		       'fail
-		       (compose-substitutions hsub tsub)
-;;;		       (handler-bind (( constraint-failure #'(lambda (c)
-;;;							       (declare (ignore c))
-;;;							       (return-from unify
-;;;								 'fail))))
-;;;			 (compose-substitutions hsub tsub))
-		       )))))))
+                 'fail
+                 (let* ((tail1 (apply-substitution (cdr e1) hsub))
+                        (tail2 (apply-substitution (cdr e2) hsub))
+                        (tsub (unify tail1 tail2)))
+                   (if (eql tsub 'fail)
+                       'fail
+                       (compose-substitutions hsub tsub)
+;;;                    (handler-bind (( constraint-failure #'(lambda (c)
+;;;                                                            (declare (ignore c))
+;;;                                                            (return-from unify
+;;;                                                              'fail))))
+;;;                      (compose-substitutions hsub tsub))
+                       )))))))
 
 (defun unify1 (e1 e2)
   (cond ((equal e1 e2) nil)
-	((variablep e1)
-	 (if (and (occurs (variable-name e1) e2))
-	     'fail
-	     (list (make-binding e1 e2))))
-	((variablep e2)
-	 (list (make-binding e2 e1)))
-	(t 'fail)))
+        ((variablep e1)
+         (if (and (occurs (variable-name e1) e2))
+             'fail
+             (list (make-binding e1 e2))))
+        ((variablep e2)
+         (list (make-binding e2 e1)))
+        (t 'fail)))
 
 ;;; OCCURS is the infamous "occurs check" - it returns T if the variable
 ;;; symbol V occurs anywhere in the expression EXPR (Dana Nau)
 (defun occurs (variable-name expr)
   (cond ((variablep expr)
-	 (eq variable-name (variable-name expr)))
-	((atom expr)
-	 ;; if it's an atom, and it's not also a variable then no...
-	 nil)
-	(t
-	 ;; should be a cons with car and cdr...
-	 (or (occurs variable-name (car expr)) (occurs variable-name (cdr expr))))))
+         (eq variable-name (variable-name expr)))
+        ((atom expr)
+         ;; if it's an atom, and it's not also a variable then no...
+         nil)
+        (t
+         ;; should be a cons with car and cdr...
+         (or (occurs variable-name (car expr)) (occurs variable-name (cdr expr))))))
 
 (defun variable-gensym (&optional base-name)
   "Return a new variable, made from BASE-NAME"
@@ -327,19 +325,19 @@ variables, with new names."
   (cond
     ((null expr) (values nil subs))
     ((consp expr)
-     (multiple-value-bind 
-	 (car-expr car-subs)
-	 (standardize (car expr) subs)
+     (multiple-value-bind
+         (car-expr car-subs)
+         (standardize (car expr) subs)
        (multiple-value-bind
-	   (cdr-expr cdr-subs)
-	   (standardize (cdr expr) car-subs)
-	 (values (cons car-expr cdr-expr) cdr-subs))))
+           (cdr-expr cdr-subs)
+           (standardize (cdr expr) car-subs)
+         (values (cons car-expr cdr-expr) cdr-subs))))
     ((variablep expr)
      (let ((bin (assoc expr subs)))
        (if bin
-	   (values (cdr bin) subs)
-	   (let ((new-var (variable-gensym expr)))
-	     (values new-var (cons (cons expr new-var) subs))))))
+           (values (cdr bin) subs)
+           (let ((new-var (variable-gensym expr)))
+             (values new-var (cons (cons expr new-var) subs))))))
     (t
      (values expr subs))))
 
@@ -352,15 +350,15 @@ variables, with new names."
 (defun fix-uninterned-bindings (bindings query-vars)
 ;(format t "~%bindings: ~s - vars: ~s" bindings query-vars)
   (mapcar #'(lambda (binding)
-	      (let* ((name (symbol-name (binding-var binding)))
-		     (matching-var
-		      (find-if 
-		       #'(lambda (v) (string-equal (symbol-name v) name))
-		       query-vars)))
-		(if matching-var
-		    (make-binding matching-var (binding-val binding))
-		  binding)))
-	  bindings))
+              (let* ((name (symbol-name (binding-var binding)))
+                     (matching-var
+                      (find-if
+                       #'(lambda (v) (string-equal (symbol-name v) name))
+                       query-vars)))
+                (if matching-var
+                    (make-binding matching-var (binding-val binding))
+                  binding)))
+          bindings))
 
 (defmethod set-variable-property ((domain t) x)
   (cond ((symbolp x)
@@ -382,13 +380,13 @@ variables, with new names."
 
 (defun get-alist (variables)
   "Takes a list of variable names as input (VARIABLES), and
-returns two values:  a binding-list mapping the VARIABLES to 
+returns two values:  a binding-list mapping the VARIABLES to
 newly created variables (made using VARIABLE-GENSYM), and
 a list of the new variables, made with VARIABLE-GENSYM."
   (let ((vlist (mapcar #'(lambda (x)
-			   (declare (ignore x))
-			   (variable-gensym))
-		       variables)))
+                           (declare (ignore x))
+                           (variable-gensym))
+                       variables)))
     (values
      (make-binding-list variables vlist)
      vlist)))
