40d31db9357e7101ad73f0d5288fbda1d2478e6d
Robert P. Goldman <rpgoldman@sift.net>
2009-10-06 22:54:33 +0000

    Bumping the +shop-version+ to 2.5.0 --- the export of shop-variable as a type means that the API has changed.

--- shop2/version.lisp
+++ shop2/version.lisp
@@ -1,6 +1,6 @@
 (in-package :cl-user)
 
-(let ((shop-version "2.4.0"))
+(let ((shop-version "2.5.0"))
   (if (boundp '+shop-version+)
       (unless (string-equal +shop-version+ shop-version)
         (cerror "Continue and reset shop version."
