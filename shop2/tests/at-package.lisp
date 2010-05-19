4622ac59ce9a6e4c212a5b4397e4ffad546d7f86
Robert P. Goldman <rpgoldman@sift.net>
2010-05-19 21:52:15 +0000

    Added block comment, and added the plural as a nickname --- too hard to remember whether it's arity-test or arity-tests, so we accept both.

--- shop2/tests/at-package.lisp
+++ shop2/tests/at-package.lisp
@@ -1,5 +1,22 @@
+;;;---------------------------------------------------------------------------
+;;; Copyright Smart Information Flow Technologies, d/b/a SIFT, LLC
+;;; All rights reserved.
+;;;
+;;; SIFT PROPRIETARY
+;;;---------------------------------------------------------------------------
+;;; File Description:
+;;;
+;;;    Package declaration for misnamed arity-tests.
+;;;
+;;; History/Bugs/Notes:
+;;;
+;;;   [2010/05/19:rpg] modified; block comment added.
+;;;
+;;;---------------------------------------------------------------------------
+
+
 (defpackage arity-test
-  (:nicknames #:at)
+  (:nicknames #:at #:arity-tests)
   (:use #:common-lisp #:shop2)
   (:import-from #:nst
                 #:def-fixtures
