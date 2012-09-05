e32ac933acadcf53235ad9226253a15d413f04fe
Robert P. Goldman <rpgoldman@sift.net>
2012-09-05 17:36:39 +0000

    Silently define the logistics-domain in LOGISTICS-DOMAIN function.

--- shop2/examples/logistic/logistic.lisp
+++ shop2/examples/logistic/logistic.lisp
@@ -41,156 +41,156 @@
 (in-package :shop2-user)
 
 (defun logistics-domain ()
+  (let ((*define-silently* t))
+    (defdomain (logistics :redefine-ok t)
+       (
+        ;; basic operators
 
-  (defdomain (logistics :redefine-ok t)
-   (
-    ;; basic operators
+        (:operator (!load-truck ?obj ?truck ?loc)
+                   ((obj-at ?obj ?loc) (:protection (truck-at ?truck ?loc)))
+                   ((in-truck ?obj ?truck)))
 
-    (:operator (!load-truck ?obj ?truck ?loc)
-               ((obj-at ?obj ?loc) (:protection (truck-at ?truck ?loc)))
-               ((in-truck ?obj ?truck)))
+        (:operator (!unload-truck ?obj ?truck ?loc)
+                   ((in-truck ?obj ?truck)
+                    (:protection (truck-at ?truck ?loc)))
+                   ((obj-at ?obj ?loc)))
 
-    (:operator (!unload-truck ?obj ?truck ?loc)
-               ((in-truck ?obj ?truck)
-                (:protection (truck-at ?truck ?loc)))
-               ((obj-at ?obj ?loc)))
+        (:operator (!load-airplane ?obj ?airplane ?loc)
+                   ((obj-at ?obj ?loc)
+                    (:protection (airplane-at ?airplane ?loc)))
+                   ((in-airplane ?obj ?airplane)))
 
-    (:operator (!load-airplane ?obj ?airplane ?loc)
-               ((obj-at ?obj ?loc)
-                (:protection (airplane-at ?airplane ?loc)))
-               ((in-airplane ?obj ?airplane)))
+        (:operator (!unload-airplane ?obj ?airplane ?loc)
+                   ((in-airplane ?obj ?airplane)
+                    (:protection (airplane-at ?airplane ?loc)))
+                   ((obj-at ?obj ?loc)))
 
-    (:operator (!unload-airplane ?obj ?airplane ?loc)
-               ((in-airplane ?obj ?airplane)
-                (:protection (airplane-at ?airplane ?loc)))
-               ((obj-at ?obj ?loc)))
+        (:operator (!drive-truck ?truck ?loc-from ?loc-to)
+                   ((truck-at ?truck ?loc-from))
+                   ((truck-at ?truck ?loc-to)
+                    (:protection (truck-at ?truck ?loc-to))))
 
-    (:operator (!drive-truck ?truck ?loc-from ?loc-to)
-               ((truck-at ?truck ?loc-from))
-               ((truck-at ?truck ?loc-to)
-                (:protection (truck-at ?truck ?loc-to))))
-
-    (:operator (!fly-airplane ?airplane ?airport-from ?airport-to)
-               ((airplane-at ?airplane ?airport-from))
-               ((airplane-at ?airplane ?airport-to)
-                (:protection (airplane-at ?airplane ?airport-to))))
+        (:operator (!fly-airplane ?airplane ?airport-from ?airport-to)
+                   ((airplane-at ?airplane ?airport-from))
+                   ((airplane-at ?airplane ?airport-to)
+                    (:protection (airplane-at ?airplane ?airport-to))))
 
 
-    ;; book-keeping methods & ops, to keep track of what needs to be done
-    ;; !add-protection and !delete-protection are two special operators
-    ;; that deal with the protection list instead of current state.
-    (:operator (!add-protection ?g)
-               ()
-               ((:protection ?g))
-               0)
+        ;; book-keeping methods & ops, to keep track of what needs to be done
+        ;; !add-protection and !delete-protection are two special operators
+        ;; that deal with the protection list instead of current state.
+        (:operator (!add-protection ?g)
+                   ()
+                   ((:protection ?g))
+                   0)
 
-    (:operator (!delete-protection ?g)
-               ((:protection ?g))
-               ()
-               0)
+        (:operator (!delete-protection ?g)
+                   ((:protection ?g))
+                   ()
+                   0)
 
     ;;;------------------------------------------------------------
 
-    ;; actual AI planning methods
+        ;; actual AI planning methods
 
-    ;; same city deliver
-    (:method (obj-at ?obj ?loc-goal)
-             same-city-deliver
-             ((in-city ?loc-goal ?city-goal)
-              (obj-at ?obj ?loc-now)
-              (in-city ?loc-now ?city-goal)
-              (truck ?truck ?city-goal)
-              ;;(truck-at ?truck ?loc-truck)
-              )
-            ((:task in-city-delivery ?truck ?obj ?loc-now ?loc-goal))
+        ;; same city deliver
+        (:method (obj-at ?obj ?loc-goal)
+           same-city-deliver
+           ((in-city ?loc-goal ?city-goal)
+            (obj-at ?obj ?loc-now)
+            (in-city ?loc-now ?city-goal)
+            (truck ?truck ?city-goal)
+            ;;(truck-at ?truck ?loc-truck)
+            )
+           ((:task in-city-delivery ?truck ?obj ?loc-now ?loc-goal))
 
-            different-city-deliver
-            ((in-city ?loc-goal ?city-goal)
-             (obj-at ?obj ?loc-now)
-             (in-city ?loc-now ?city-now)
-             (different ?city-goal ?city-now)
-             (truck ?truck-now ?city-now)
-             (truck ?truck-goal ?city-goal)
-             (airport ?airport-now) (in-city ?airport-now ?city-now)
-             (airport ?airport-goal) (in-city ?airport-goal ?city-goal))
-            (:ordered (:task in-city-delivery ?truck-now ?obj 
-?loc-now ?airport-now)
-                      (:task air-deliver-obj ?obj ?airport-now ?airport-goal)
-                      (:task in-city-delivery ?truck-goal ?obj 
-?airport-goal ?loc-goal)))
-
-    ;;;-------------------------------------------------
-    (:method (in-city-delivery ?truck ?obj ?loc-from ?loc-to)
-
-             package-already-there
-             ((same ?loc-from ?loc-to))
-             ()
-
-             truck-across-town
-             ((in-city ?loc-from ?city)
-              (truck ?truck ?city))
-             (:ordered (:task truck-at ?truck ?loc-from)
-                       (:task :immediate !load-truck ?obj ?truck ?loc-from)
-                       (:task truck-at ?truck ?loc-to)
-                       (:task :immediate !unload-truck ?obj ?truck ?loc-to)))
-
+           different-city-deliver
+           ((in-city ?loc-goal ?city-goal)
+            (obj-at ?obj ?loc-now)
+            (in-city ?loc-now ?city-now)
+            (different ?city-goal ?city-now)
+            (truck ?truck-now ?city-now)
+            (truck ?truck-goal ?city-goal)
+            (airport ?airport-now) (in-city ?airport-now ?city-now)
+            (airport ?airport-goal) (in-city ?airport-goal ?city-goal))
+           (:ordered (:task in-city-delivery ?truck-now ?obj 
+                            ?loc-now ?airport-now)
+               (:task air-deliver-obj ?obj ?airport-now ?airport-goal)
+               (:task in-city-delivery ?truck-goal ?obj 
+                      ?airport-goal ?loc-goal)))
 
     ;;;-------------------------------------------------
+        (:method (in-city-delivery ?truck ?obj ?loc-from ?loc-to)
 
-    (:method (truck-at ?truck ?loc-to)
+           package-already-there
+           ((same ?loc-from ?loc-to))
+           ()
 
-             truck-not-in-right-location
-             ((truck-at ?truck ?loc-from)
-              (different ?loc-from ?loc-to))
-             ((:task :immediate !drive-truck ?truck ?loc-from ?loc-to))
+           truck-across-town
+           ((in-city ?loc-from ?city)
+            (truck ?truck ?city))
+           (:ordered (:task truck-at ?truck ?loc-from)
+               (:task :immediate !load-truck ?obj ?truck ?loc-from)
+               (:task truck-at ?truck ?loc-to)
+             (:task :immediate !unload-truck ?obj ?truck ?loc-to)))
 
-             truck-in-right-location
-             ((truck-at ?truck ?loc-from)
-              (same ?loc-from ?loc-to))
-             ((:task :immediate !add-protection (truck-at ?truck ?loc-to))))
+
+    ;;;-------------------------------------------------
+
+        (:method (truck-at ?truck ?loc-to)
+
+           truck-not-in-right-location
+           ((truck-at ?truck ?loc-from)
+            (different ?loc-from ?loc-to))
+           ((:task :immediate !drive-truck ?truck ?loc-from ?loc-to))
+
+           truck-in-right-location
+           ((truck-at ?truck ?loc-from)
+            (same ?loc-from ?loc-to))
+           ((:task :immediate !add-protection (truck-at ?truck ?loc-to))))
 
     ;;; (:method head [name] conjunct tail [[name] conjunct tail])
 
     ;;;-------------------------------------------------
-    (:method (air-deliver-obj ?obj ?airport-from ?airport-to)
-             airplane-at-the-current-airport
-             ((airplane-at ?airplane ?airport-from))
-             (:ordered (:task :immediate !add-protection (airplane-at 
-?airplane ?airport-from))
-                       (:task !load-airplane ?obj ?airplane ?airport-from)
-                       (:task fly-airplane ?airplane ?airport-to)
-                       (:task !unload-airplane ?obj ?airplane ?airport-to))
+        (:method (air-deliver-obj ?obj ?airport-from ?airport-to)
+           airplane-at-the-current-airport
+           ((airplane-at ?airplane ?airport-from))
+           (:ordered (:task :immediate !add-protection (airplane-at 
+                                                        ?airplane ?airport-from))
+               (:task !load-airplane ?obj ?airplane ?airport-from)
+               (:task fly-airplane ?airplane ?airport-to)
+             (:task !unload-airplane ?obj ?airplane ?airport-to))
 
-             ;; no airplane at the current airport
-             fly-airplane-from-any-other-airport
-             ((airplane-at ?airplane ?any-airport))
-             (:ordered (:task :immediate !fly-airplane ?airplane 
-?any-airport ?airport-from)
-                       (:task !load-airplane ?obj ?airplane ?airport-from)
-                       (:task fly-airplane ?airplane ?airport-to)
-                       (:task !unload-airplane ?obj ?airplane ?airport-to)))
+           ;; no airplane at the current airport
+           fly-airplane-from-any-other-airport
+           ((airplane-at ?airplane ?any-airport))
+           (:ordered (:task :immediate !fly-airplane ?airplane 
+                            ?any-airport ?airport-from)
+               (:task !load-airplane ?obj ?airplane ?airport-from)
+               (:task fly-airplane ?airplane ?airport-to)
+             (:task !unload-airplane ?obj ?airplane ?airport-to)))
 
-    (:method (fly-airplane ?airplane ?airport-to)
-             airplane-already-there
-             ((airplane-at ?airplane ?airport-to))
-             ((:task :immediate !add-protection (airplane-at ?airplane 
-?airport-to)))
+        (:method (fly-airplane ?airplane ?airport-to)
+           airplane-already-there
+           ((airplane-at ?airplane ?airport-to))
+           ((:task :immediate !add-protection (airplane-at ?airplane 
+                                                           ?airport-to)))
 
-             fly-airplane-in
-             ((airplane-at ?airplane ?airport-from))
-             ((:task :immediate !fly-airplane ?airplane ?airport-from 
-?airport-to)))
+           fly-airplane-in
+           ((airplane-at ?airplane ?airport-from))
+           ((:task :immediate !fly-airplane ?airplane ?airport-from 
+                   ?airport-to)))
 
 
     ;;;-------------------------------------------------
 
-    ;; state axioms
+        ;; state axioms
 
-    (:- (same ?x ?x) nil)
-    (:- (different ?x ?y) ((not (same ?x ?y))))
+        (:- (same ?x ?x) nil)
+        (:- (different ?x ?y) ((not (same ?x ?y))))
 
 
-    )))
+        ))))
 
 (eval-when (:load-toplevel)
   (logistics-domain))
