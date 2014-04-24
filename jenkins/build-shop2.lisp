00c3aacd7dfa14064be8ca39ba13d1fb52b467f2
Robert P. Goldman <rpgoldman@sift.net>
2014-04-24 17:29:17 +0000

    Partial draft of intolerant SHOP2 build script.

---
+++ jenkins/build-shop2.lisp
@@ -0,0 +1,73 @@
+(in-package :common-lisp-user)
+
+;; git checkout 
+git clone git://common-lisp.net/projects/asdf/asdf.git
+
+(defparameter *shop-root-directory*
+  (merge-pathnames
+   (make-pathname :directory '(:relative :back)
+                  :name nil :type nil)
+   *load-truename*))
+
+(format t "*SHOP-ROOT-DIRECTORY* is:")
+(describe *shop-root-directory*)
+
+(asdf:initialize-source-registry
+ `(:source-registry (:tree ,*shop-root-directory*)
+                    :inherit-configuration))
+
+;;; code adapted from cl-launch http://www.cliki.net/cl-launch
+(defun exit-lisp (return)
+  #+allegro
+  (excl:exit return)
+  #+clisp
+  (ext:quit return)
+  #+(or cmu scl)
+  (unix:unix-exit return)
+  #+ecl
+  (si:quit return)
+  #+gcl
+  (lisp:quit return)
+  #+lispworks
+  (lispworks:quit :status return :confirm nil :return nil :ignore-errors-p t)
+  #+(or openmcl mcl)
+  (ccl::quit return)
+  #+mkcl
+  (mk-ext:quit :exit-code return)
+  #+sbcl #.(let ((exit (find-symbol "EXIT" :sb-ext))
+                 (quit (find-symbol "QUIT" :sb-ext)))
+             (cond
+               (exit `(,exit :code return :abort t))
+               (quit `(,quit :unix-status return :recklessly-p t))))
+  #+(or abcl xcl)
+  (ext:quit :status return)
+  (error "Don't know how to quit Lisp; wanting to use exit code ~a" return))
+
+(setf asdf::*compile-file-failure-behaviour* :error)
+(setf asdf::*compile-file-warnings-behaviour* :warn)
+
+(defvar *build-warning* nil)
+(defvar *build-error* nil)
+
+(handler-bind ((warning #'(lambda (x) (declare (ignore x)) 
+                            (setf *build-warning* t)))
+               (error #'(lambda (x) (declare (ignore x))
+                          (setf *build-error* t))))
+  (asdf:load-system "shop2" :force :all))
+
+(cond
+  (*build-error*
+   (format t "SHOP2 build failed with an error.~%")
+   (exit-lisp 1))
+  (*build-warning*
+   (format t "SHOP2 build failed with a warning.~%")
+   (exit-lisp 2))
+  (t
+   (format t "SHOP2 build successful.~%")
+   (exit-lisp 0)))
+
+
+
+
+
+
