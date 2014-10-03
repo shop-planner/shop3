683930831c6852a923e0a9d8ba8d5e225c3e77c5
Robert P. Goldman <rpgoldman@sift.net>
2014-10-03 21:15:44 +0000

    Added complex wrinkle that avoids spurious build failures on SBCL.

--- jenkins/build-shop2.lisp
+++ jenkins/build-shop2.lisp
@@ -4,33 +4,6 @@
 
 (declaim (optimize (speed 3) (space 3) (safety 3)))
 
-;;; code adapted from cl-launch http://www.cliki.net/cl-launch
-(defun exit-lisp (return)
-  #+allegro
-  (excl:exit return)
-  #+clisp
-  (ext:quit return)
-  #+(or cmu scl)
-  (unix:unix-exit return)
-  #+ecl
-  (si:quit return)
-  #+gcl
-  (lisp:quit return)
-  #+lispworks
-  (lispworks:quit :status return :confirm nil :return nil :ignore-errors-p t)
-  #+(or openmcl mcl)
-  (ccl::quit return)
-  #+mkcl
-  (mk-ext:quit :exit-code return)
-  #+sbcl #.(let ((exit (find-symbol "EXIT" :sb-ext))
-                 (quit (find-symbol "QUIT" :sb-ext)))
-             (cond
-               (exit `(,exit :code return :abort t))
-               (quit `(,quit :unix-status return :recklessly-p t))))
-  #+(or abcl xcl)
-  (ext:quit :status return)
-  (error "Don't know how to quit Lisp; wanting to use exit code ~a" return))
-
 (asdf:load-system "asdf")
 ;; (asdf:initialize-output-translations)
 ;; (asdf:initialize-source-registry)
@@ -42,25 +15,21 @@
 (defvar *build-warning* nil)
 (defvar *build-error* nil)
 
-(handler-bind ((warning #'(lambda (x) (declare (ignore x)) 
-                            (setf *build-warning* t)))
-               (error #'(lambda (x) (declare (ignore x))
+(handler-bind ((warning #'(lambda (x)
+                            ;; this is necessary because on SBCL
+                            ;; there's an EXTERNAL handler for some
+                            ;; uninteresting warnings.
+                            (signal x)
+                            (push x *build-warning*)))
+               (error #'(lambda (x)
                           (setf *build-error* t))))
   (asdf:load-system "shop2" :force :all))
 
 (cond
   (*build-error*
-   (format t "SHOP2 build failed with an error.~%")
-   (exit-lisp 1))
+   (uiop:die 1 "SHOP2 build failed with an error: ~a.~%" *build-error*))
   (*build-warning*
-   (format t "SHOP2 build failed with a warning.~%")
-   (exit-lisp 2))
+   (uiop:die 2 "SHOP2 build failed with warnings:~%~{~t~a~%~}" *build-warning*))
   (t
    (format t "SHOP2 build successful.~%")
-   (exit-lisp 0)))
-
-
-
-
-
-
+   (uiop:quit 0)))
