75f9ed49d6b156cf35b53b32ecbee3d8514f7603
Robert P. Goldman <rpgoldman@sift.net>
2015-07-06 16:06:48 +0000

    Summary: Add test for ticket:261. Show that IF-THEN-ELSE semantics of axioms does not perform correctly across task backtracks.

--- shop2/tests/misc.lisp
+++ shop2/tests/misc.lisp
@@ -17,6 +17,48 @@
                       (format x "~a" c))))))
          (find-plans prob-name)))
       (format nil "No such problem: ~a" prob-name)))))
-              
-             
+
+(define-condition %bad-backtrack-cond (error)
+  ()
+  (:report (lambda (c s)
+             (format s "Encountered bad backtrack condition in SHOP2."))))
+
+(defun test-backtrack-domain ()
+  (defdomain test-backtrack
+    ((:method (example-with-backtrack ?x)
+       ((foo ?x)
+        (bar ?_y))
+       ())
+     (:- (= ?x ?x)
+         ())
+     (:- (foo ?x)
+         ((= ?x 2)
+          (eval (eql ?x 2)))
+         ((eval (error '%bad-backtrack-cond)))))))
+
+(defun bad-backtrack-problem ()
+  (make-problem 'bad-backtrack-problem 'test-backtrack
+                nil
+                '(example-with-backtrack 2)))
+
+(fiveam:test check-bad-backtrack-domain
+  (test-backtrack-domain)
+  (make-problem 'check-bad-backtrack-domain 'test-backtrack
+                '((bar 22))
+                '(example-with-backtrack 2))
+  (unwind-protect
+       (fiveam:is-true (find-plans 'check-bad-backtrack-domain))
+    (shop2:delete-problem 'check-bad-backtrack-domain)
+    ;; FIXME: there should be a DELETE-DOMAIN, but there isn't...
+    ))
+  
+;;; To verify SHOP2 ticket:261 (https://svn.sift.info:3333/trac/shop2/ticket/261) do the following:
+#+nil
+(progn
+  (test-backtrack-domain)
+  (bad-backtrack-problem)
+  (find-plans 'bad-backtrack-problem))
+;;; this will raise an error as we somehow backtrack into the error call, which
+;;; should be unreachable.
+
       
