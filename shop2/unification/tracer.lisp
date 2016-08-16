fa29a8fe3f2a85b76bcf953281e78e90be46edf8
Robert P. Goldman <rpgoldman@sift.net>
2016-08-16 21:06:59 +0000

    Canonicalize whitespace.
    No tabs, no trailing whitespace.

--- shop2/unification/tracer.lisp
+++ shop2/unification/tracer.lisp
@@ -1,19 +1,19 @@
 ;;; -*- Mode: common-lisp; package: shop2.unifier; -*-
 ;;;
 ;;; Version: MPL 1.1/GPL 2.0/LGPL 2.1
-;;; 
+;;;
 ;;; The contents of this file are subject to the Mozilla Public License
 ;;; Version 1.1 (the "License"); you may not use this file except in
 ;;; compliance with the License. You may obtain a copy of the License at
 ;;; http://www.mozilla.org/MPL/
-;;; 
+;;;
 ;;; Software distributed under the License is distributed on an "AS IS"
 ;;; basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 ;;; License for the specific language governing rights and limitations under
 ;;; the License.
-;;; 
-;;; The Original Code is SHOP2.  
-;;; 
+;;;
+;;; The Original Code is SHOP2.
+;;;
 ;;; The Initial Developer of the Original Code is the University of
 ;;; Maryland. Portions created by the Initial Developer are Copyright (C)
 ;;; 2002,2003 the Initial Developer. All Rights Reserved.
@@ -22,8 +22,8 @@
 ;;; Portions created by Drs. Goldman and Maraist are Copyright (C)
 ;;; 2004-2007 SIFT, LLC.  These additions and modifications are also
 ;;; available under the MPL/GPL/LGPL licensing terms.
-;;; 
-;;; 
+;;;
+;;;
 ;;; Alternatively, the contents of this file may be used under the terms of
 ;;; either of the GNU General Public License Version 2 or later (the "GPL"),
 ;;; or the GNU Lesser General Public License Version 2.1 or later (the
@@ -39,16 +39,16 @@
 ;;; ----------------------------------------------------------------------
 
 ;;; Smart Information Flow Technologies Copyright 2006-2007 Unpublished work
-;;; 
+;;;
 ;;; GOVERNMENT PURPOSE RIGHTS
-;;; 
-;;; Contract No.         FA8650-06-C-7606, 
+;;;
+;;; Contract No.         FA8650-06-C-7606,
 ;;; Contractor Name      Smart Information Flow Technologies, LLC
 ;;;                      d/b/a SIFT, LLC
 ;;; Contractor Address   211 N 1st Street, Suite 300
 ;;;                      Minneapolis, MN 55401
 ;;; Expiration Date      5/2/2011
-;;; 
+;;;
 ;;; The Government's rights to use, modify, reproduce, release,
 ;;; perform, display, or disclose this software are restricted by
 ;;; paragraph (b)(2) of the Rights in Noncommercial Computer Software
@@ -67,17 +67,17 @@
   `(when *shop-trace*
      (when (or (member ,type *shop-trace*) (member ',item *shop-trace*))
        ,(let ((cpack (find-package :shop2.common)))
-	  (when cpack
-	    (let ((s-a (intern (symbol-name '#:state-atoms) cpack)))
-	      (when (fboundp s-a)
-		`(when *trace-query*
-		   (funcall (fdefinition 'trace-query-hook) ,type ,item
-			    (list ,@formats) (,s-a ,state)))))))       
+          (when cpack
+            (let ((s-a (intern (symbol-name '#:state-atoms) cpack)))
+              (when (fboundp s-a)
+                `(when *trace-query*
+                   (funcall (fdefinition 'trace-query-hook) ,type ,item
+                            (list ,@formats) (,s-a ,state)))))))
        (format *shop-trace-stream* ,@formats)
        ,(let ((cpack (find-package :shop2.common)))
-	  (when cpack
-	    (let ((s-a (intern (symbol-name '#:state-atoms) cpack)))
-	      (when (fboundp s-a)
-		`(when (member :states *shop-trace*)
-		   (format *shop-trace-stream* "~%     state ~s"
-		     (,s-a ,state))))))))))
+          (when cpack
+            (let ((s-a (intern (symbol-name '#:state-atoms) cpack)))
+              (when (fboundp s-a)
+                `(when (member :states *shop-trace*)
+                   (format *shop-trace-stream* "~%     state ~s"
+                     (,s-a ,state))))))))))
