596b22e7f037fa920d66de0dce9c1abe4fdad68b
Robert P. Goldman <rpgoldman@sift.net>
2017-03-30 18:27:12 +0000

    Give SBCL more memory to pass the Jenkins tests.

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -155,7 +155,7 @@ case "$lisp" in
     eval="-eval" ;;
   sbcl)
     command="${SBCL:-sbcl}"
-    flags="--noinform --userinit /dev/null --sysinit /dev/null"
+    flags="--dynamic-space-size 4000 --noinform --end-runtime-options --no-userinit --no-sysinit"
     nodebug="--disable-debugger"
     eval="--eval" ;;
   # scl)
