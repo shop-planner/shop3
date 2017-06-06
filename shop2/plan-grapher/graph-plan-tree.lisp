8582e8b31079788b3945d1de487079a7daea2f9d
Robert P. Goldman <rpgoldman@sift.net>
2017-06-06 19:56:34 +0000

    Support the option of cross-links for orderings of primitive nodes.

--- shop2/plan-grapher/graph-plan-tree.lisp
+++ shop2/plan-grapher/graph-plan-tree.lisp
@@ -1,5 +1,8 @@
 (in-package :spg)
 
+(defvar modified-forest) ; used as special so that we can draw
+                         ; cross-edges between primitive nodes.
+
 (defun graph-plan-tree (plan-forest &key (tree-processor 'identity)
                                          (graph-object (make-instance 'plan-tree-graph)))
   "Takes a SHOP2 plan forest (PLAN-TREE) as input, and returns a CL-DOT graph object.
@@ -19,10 +22,40 @@ If you ask for plan trees from SHOP2, you really get plan *forests*."
 
 (defmethod cl-dot:graph-object-points-to ((g plan-tree-graph)(obj cons))
   (declare (ignorable g))
-  (when (complex-node-p obj)
-    (reverse
-     (complex-node-children obj))))
+  (cond ((complex-node-p obj)
+         (reverse
+          (complex-node-children obj)))
+        ((primitive-node-p obj)
+         (let ((next-node (find-next-node (primitive-node-position obj) modified-forest)))
+           (when next-node
+             (list 
+              (make-instance 'cl-dot:attributed
+                            :object next-node
+                            :attributes `(:constraint nil :color :blue))))))
+        (t (error 'type-error :type-error-datum obj
+                              :type-error-expected-type '(or primitive-node complex-node)))))
 
+(defun find-next-node (index forest)
+  (let (next-node
+        (next-node-index most-positive-fixnum))
+    (labels ((find-next (tree)
+               (cond ((complex-node-p tree)
+                      (mapc 'find-next (complex-node-children tree)))
+                     ((primitive-node-p tree)
+                      (let ((pos (primitive-node-position tree)))
+                        (when (and (> pos index)
+                                   (< pos next-node-index))
+                          (setf next-node tree
+                                next-node-index pos))))
+                     (t (error 'type-error :type-error-datum tree
+                                           :type-error-expected-type '(or primitive-node complex-node))))))
+      (dolist (tree forest)
+        (find-next tree)
+        (when (and next-node
+                   (= next-node-index (1+ index)))
+          (return-from find-next-node next-node)))
+      next-node)))
+         
 (defvar *label-depends*)
 
 (defun graph-enhanced-plan-tree (plan-tree &key 
