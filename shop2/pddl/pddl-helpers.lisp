4436b971f3016f7976cd1c9074306059ed954d26
Robert P. Goldman <rpgoldman@sift.net>
2017-09-12 22:01:40 +0000

    Fix INTERN bug found by SBCL.
    SBCL didn't like my using INTERN with string designators and package
    designators, instead of strings and packages.
    Easy fix was to replace with calls to UIOP:INTERN*

--- shop2/pddl/pddl-helpers.lisp
+++ shop2/pddl/pddl-helpers.lisp
@@ -20,7 +20,9 @@
   (:shadowing-import-from #:shop2
                           #:domain-name #:make-problem #:domain)
   (:export #:typed-object-list->facts
-           #:translate-openstacks-problem))
+           #:translate-openstacks-problem
+           #:check-repair
+           #:make-divergence-operator))
 
 (in-package #:shop2-pddl-helpers)
 
@@ -35,7 +37,7 @@
     (let ((problem 
             (pddl-utils:read-pddl-file problem-file)))
       (make-problem (pddl-utils:problem-name problem)
-                    (intern '#:openstacks-sequencedstrips-ADL-included
+                    (uiop:Intern* '#:openstacks-sequencedstrips-ADL-included
                             package)
                     ;; state
                     (append
@@ -47,4 +49,52 @@
                     ;; tasks
                     '(shop2-openstacks::plan)))))
 
+(defun make-divergence-operator (divergence &key (package pddl-utils:*pddl-package*))
+  (assert (eq  (first divergence) :divergence))
+  (let ((effects 
+          (iter (for (op literal) in (rest divergence))
+            (collecting
+             (ecase op
+               (:add literal)
+               (:delete `(not ,literal)))))))
+    (make-action (uiop:Intern* '#:divergence package)
+                 nil                    ;parameters
+                 :effect `(and
+                           ,@effects))))
 
+;;; FIXME: later make KEEP default to NIL
+(defun check-repair (orig-domain orig-problem repaired-plan &key (package *package*) (keep t))
+  (let ((pddl-utils:*pddl-package* package))
+    (let* ((domain (if (or (stringp orig-domain) (pathnamep orig-domain))
+                           (read-pddl-file orig-domain)
+                           ;; don't need to copy here, because INSERT-DOMAIN-ACTIONS takes care of that
+                            orig-domain))
+           (divergence-pos (position :divergence repaired-plan
+                                          :key #'(lambda (x) (and (listp x) (first x)))))
+           (divergence (nth divergence-pos repaired-plan))
+           (repaired-plan (let ((new (copy-list repaired-plan)))
+                            (setf (nth divergence-pos new)
+                                  (list (uiop:Intern* '#:divergence package)))
+                            new))
+           (problem-file
+             (if (or (stringp orig-problem) (pathnamep orig-problem))
+                 orig-problem
+                 (uiop:with-temporary-file (:stream str :pathname path :keep t)
+                   (pprint-pddl orig-problem str)
+                   path)))
+           (domain-file
+             (uiop:with-temporary-file (:stream str :pathname path :keep t)
+               (let ((expanded 
+                       (insert-domain-actions domain (list (make-divergence-operator divergence)))))
+                 (pprint-pddl expanded str))
+               path)))
+      (break "Check repaired plan")
+      (unwind-protect
+           (values
+            (validate-plan repaired-plan domain-file problem-file)
+            problem-file
+            domain-file)
+        (unless keep
+          (uiop:delete-file-if-exists domain-file)
+          (unless (or (stringp orig-problem) (pathnamep orig-problem))
+            (uiop:delete-file-if-exists problem-file)))))))
