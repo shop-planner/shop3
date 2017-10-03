d7be713452d6f8337ea1b25152460a3c6dc02786
Robert P. Goldman <rpgoldman@sift.net>
2017-10-03 21:48:04 +0000

    Improvements to plan repair and preparation for validation.

--- shop2/explicit-stack-search/plan-repair.lisp
+++ shop2/explicit-stack-search/plan-repair.lisp
@@ -15,20 +15,24 @@ Returns: (1) new plan (2) new plan tree (enhanced plan tree, not old-style SHOP 
 \(3\) plan tree lookup table (4) search-state object."
   (let ((failed (subtree:find-failed-task domain plan plan-tree executed
                                                  divergence :plan-tree-hash plan-tree-hash))
-        (new-search-state (shop2::freeze-state executed divergence search-state)))
+        (new-search-state (freeze-state executed divergence search-state)))
+    #+nil(break "Inspect NEW-SEARCH-STATE.")
     (multiple-value-bind (new-plans new-plan-trees lookup-tables final-search-state)
         (replan-from-failure domain failed new-search-state :verbose verbose)
-      (let ((new-plan (first new-plans))
-            (new-plan-tree (first new-plan-trees))
-            (new-lookup-table (first lookup-tables)))
-        (values
-         ;; new plan sequence
-         (append executed
-                 (list (cons :divergence divergence) 0.0)
-                 (remove-if #'(lambda (x) (member x executed)) new-plan))
-         new-plan-tree
-         new-lookup-table
-         final-search-state)))))
+      (when new-plans
+        (let ((new-plan (first new-plans))
+               (new-plan-tree (first new-plan-trees))
+               (new-lookup-table (first lookup-tables)))
+          (multiple-value-bind (prefix suffix)
+              (extract-suffix new-plan executed)
+           (values
+            ;; new plan sequence
+            (append prefix
+                    (list (cons :divergence divergence) 0.0)
+                    suffix)
+            new-plan-tree
+            new-lookup-table
+            final-search-state)))))))
 
 (defgeneric find-failed-stack-entry (failed obj)
   (:documentation "Find and return the stack entry that corresponds
@@ -80,7 +84,44 @@ before the insertion of FAILED into the plan tree.")
       ;; choices.  So what we need to do is to reset the alternatives
       ;; for solving this task, not find a different task.
       (setf (mode search-state) 'expand-task)
-      (seek-plans-stack search-state domain))))
+      ;; must be "repairable" so that we don't strip NOPs.
+      (seek-plans-stack search-state domain :repairable t))))
+
+(defun extract-suffix (plan prefix)
+  ;; hardened to handle costs... yuck
+  (let ((plan-copy (copy-list plan)))
+   (iter (for x in prefix)
+     (as y = (first plan-copy))
+     (if (numberp x)
+         (cond ((eql x y)               ; match a number
+                (pop plan-copy)
+                (collecting y into new-prefix))
+               ((listp y) ; x is a cost but y is an action - do nothing
+                )
+               (t (error "Mismatching prefix ~s of ~s" x plan-copy)))
+         ;; x is an operator/action
+         (cond ((eq x y)
+                (pop plan-copy)
+                (collecting y into new-prefix))                     ; match
+               ((numberp y)
+                (pop plan-copy)
+                (let ((y (first plan-copy)))
+                  (if (eq x y)
+                      (progn (pop plan-copy)
+                             (collecting y into new-prefix))
+                      (error "Mismatch in plan: ~s ~s" x plan-copy))))
+               (t (error "Mismatching prefix ~s of ~s" x plan-copy))))
+     (finally
+      (cond ((and (numberp (first plan-copy))
+                   (some 'numberp (rest plan-copy)))
+             ;; move cost of last executed action
+             (let ((last-cost (pop plan-copy)))
+               (setf new-prefix (append new-prefix (list last-cost)))))
+            ((numberp (first plan-copy))
+             ;; there are no more numbers in the plan
+             (pop plan-copy)))
+      (return
+        (values new-prefix plan-copy))))))
 
 (defun freeze-state (executed divergence search-state)
   "Arguments:
@@ -96,22 +137,31 @@ Modified search state object."
          ;; this is the tag or the "failed" action -- really the one
          ;; that gave an unexpected result.  So we want to undo all the
          ;; actions AFTER this one
-         (world-state-tag (length executed))
+         (world-state-tag (if (some #'numberp executed)
+                              (/ (length executed) 2)
+                              (length executed)))
          (new-state-obj (shop2.common::copy-state world-state)))
+    (assert (integerp world-state-tag))
+    #+NIL(break "Inside FREEZE-STATE")
     ;; this gives us the state right after the execution of the "failed" action.
-    (shop2.common:retract-state-changes new-state-obj (1+ world-state-tag))
+    (shop2.common:retract-state-changes new-state-obj world-state-tag)
     ;; now put the divergences into effect....
     (iter (for (op fact) in divergence)
       (ecase op
         (:add (shop2.common:add-atom-to-state fact new-state-obj 0 :execution-divergence))
         (:delete (shop2.common:delete-atom-from-state fact new-state-obj 0 :execution-divergence))))
     ;; now make it impossible to backtrack before this point...
-    (setf (shop2.common::tagged-state-block-at new-state-obj) (1+ world-state-tag))
+    (setf (shop2.common::tagged-state-block-at new-state-obj) world-state-tag)
     ;; now roll forward again
-    (let ((suffix (copy-seq (rest (member world-state-tag (shop2.common::tagged-state-tags-info world-state)
-                                     :key 'first)))))
-      (alexandria:appendf (shop2.common::tagged-state-tags-info new-state-obj)
-                          suffix))
+    (let ((suffix 
+            (subseq (shop2.common::tagged-state-tags-info world-state) 0
+                    (or (position world-state-tag (shop2.common::tagged-state-tags-info world-state)
+                                  :key 'first)
+                        (error "Couldn't find world state tag ~D in state" world-state-tag)))))
+      (setf (shop2.common::tagged-state-tags-info new-state-obj)
+            ;; NCONC is safe because SUBSEQ creates fresh copy.
+            (nconc suffix (shop2.common::tagged-state-tags-info new-state-obj))))
+    (break "Inspect NEW-STATE-OBJ.")
     ;; now put the new world state in place...
     (setf (world-state search-state) new-state-obj)
-    search-state))
+    search-state)) 
