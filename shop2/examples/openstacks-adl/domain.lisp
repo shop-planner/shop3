aa284bf3768df4d44cb8cc02a110e76e5885e3de
Robert P. Goldman <rpgoldman@sift.net>
2018-01-09 02:38:58 +0000

    Replace with debugged domain definition.

--- shop2/examples/openstacks-adl/domain.lisp
+++ shop2/examples/openstacks-adl/domain.lisp
@@ -176,77 +176,145 @@
      )
   )
 
-(defdomain (openstacks-sequencedstrips-ADL-included :type pddl-domain)
+(defclass pure-pddl-domain (pddl-domain pure-logic-domain-mixin)
+  ()
+  )
+
+(defdomain (openstacks-sequencedstrips-ADL-included :type pure-pddl-domain :source-pddl-domain #.(merge-pathnames "domain-nocosts.pddl" *load-truename*))
     ((:include  openstacks-sequencedstrips-ADL-nocosts "domain-nocosts.pddl")
-     (:static includes)
-     (:method (assert-goals nil)
-       ()
-       ())
-
-     (:method (assert-goals (?goal . ?goals))
-       ()
-       (:ordered (!!assert (goal ?goal))
-                 (assert-goals ?goals))
-       )
-
-     (:method (plan)
-       ((:goal (and . ?goals)))
-       ((assert-goals ?goals)
+     (:static includes next-count)
+     
+     (:pddl-method (plan)
+       (exists (?o - order)
+               (not (shipped ?o)))
+       (:ordered
         (open-all-stacks)
         (plan-for-goals)))
 
-     (:method (open-all-stacks)
-       ((stacks-avail ?n)
-        (next-count ?n ?n1))
-       (:ordered (!open-new-stack ?n ?n1)
-                 (open-all-stacks))
-       ()
-       ()
-       )
+     (:pddl-method (open-all-stacks)
+       open-a-stack-and-recurse
+       (exists (?n ?n1 - count)
+               (and (stacks-avail ?n)
+                    (next-count ?n ?n1)))
+       (:ordered (open-new-stack ?n ?n1)
+                 (open-all-stacks)))
 
-     (:method (plan-for-goals)
-       ((goal (shipped ?order))
-        (not (shipped ?order)))
-       (:ordered (one-step) (plan-for-goals))
-       ()
-       ((verify-orders)))
+     (:pddl-method (open-all-stacks)
+       done-opening-stacks
+       (and (stacks-avail ?n)
+            ;; FIXME: should implement rewriting of negated existentials...
+            #+ignore
+            (not (exists (?n1 - count)
+                         (next-count ?n ?n1)))
+            (forall (?n1 - count)
+              (not (next-count ?n ?n1))))
+       ())
+
+     (:pddl-method (open-new-stack ?n ?n1)
+        (and (stacks-avail ?n)
+             (next-count ?n ?n1))
+        (!open-new-stack ?n ?n1))       
+
+     (:pddl-method (plan-for-goals)
+       ship-one-order
+       (exists (?o - order)
+               (not (shipped ?o)))
+       (:ordered (one-step) (plan-for-goals)))
+
+     (:pddl-method (plan-for-goals)
+       done-shipping-orders
+       (forall (?order - order) 
+               (shipped ?order))
+       ())
 
      (:method (one-step)
        ship-order
        ;; prefer to ship an order, if possible...
-       ((goal (shipped ?o))
-        (not (shipped ?o))
-        (forall (?p) (includes ?o ?p) (made ?p)))
-       ((ship-products ?o))
-       make-product
+       (and (order ?o)
+            (not (shipped ?o))
+            ;; this is a kind of cheaty way to avoid the FORALL dependency check.
+            (ready-to-ship ?o)
+            (started ?o)
+            #+ignore (forall ?p (includes ?o ?p) (made ?p)))
+       (ship-an-order ?o))
+
+     (:method (one-step)
+       repair-an-order
+       (and (order ?o)
+            (not (shipped ?o))
+            (not (started ?o))
+            (ready-to-ship ?o)
+            (next-count ?avail-prime ?avail))
+       (!start-order ?o ?avail ?avail-prime))
+
+     (:pddl-method (one-step)
+       (forall (?order - order)
+         (imply (not (shipped ?order))
+                (exists (?p - product)
+                   (and (includes ?order ?p)
+                        (not (made ?p))))))
+       (make-a-product))
+
+     ;; here's a problem -- we want the product to be made only if
+     ;; there are no products we can ship.  But we can't make that a
+     ;; precondition for this method.
+     (:method (make-a-product)
        (:sort-by ?h
-                 (and (goal (shipped ?o))
-                      (not (shipped ?o))
-                      (includes ?o ?p)
-                      (not (made ?p))
-                      (ship-cost-heuristic ?p ?h)))
+                 (and
+                  (includes ?o ?p)
+                  (not (shipped ?o))
+                  (not (made ?p))
+                  (ship-cost-heuristic ?p ?h)))
        ((make-product ?p)))
+
+     (:method (ship-an-order ?order)
+       (and (order ?order)
+            (not (shipped ?order))
+            (forall ?p (includes ?order ?p) (made ?p))
+            (stacks-avail ?avail)
+            (next-count ?avail ?new))
+       (!ship-order ?order ?avail ?new))
 
      (:method (make-product ?p)
        ()
        (:ordered (start-orders ?p)
                  (!make-product ?p)))
 
-     (:method (start-orders ?p)
+     (:pddl-method (start-orders ?p)
+       start-an-order-and-recurse
+       (exists (?o - order)
+         (and (includes ?o ?p)
+              (not (started ?o))))
+       (:ordered (start-an-order-for ?p ?order)
+        (start-orders ?p)))
+
+     (:method (start-an-order-for ?p ?o)
        ((includes ?o ?p)
-        (not (started ?o)))
-       ((start-an-order ?o)
-        (start-orders ?p))
-       done
-       ()
+        (not (started ?o))
+        (stacks-avail ?avail)
+        (next-count ?avail-prime ?avail))
+       (!start-order ?o ?avail ?avail-prime))
+
+     (:pddl-method (start-orders ?p)
+       done-starting-orders
+       (forall (?o - order)
+               (imply (includes ?o ?p)
+                      (started ?o)))
        ())
 
+#|
      (:method (verify-orders)
-       ((goal (shipped ?order))
-        (not (shipped ?order)))
-       (:eval (error "complete plan does not satisfy goals.  State is:" shop2:*current-state*))
-       ()
+       orders-incorrect
+       ((exists (?order - order) 
+          (not (shipped ?order))))
+       (:eval (error "complete plan does not satisfy goals.  State is:" shop2:*current-state*)))
+
+     (:method (verify-orders)
+       orders-correct
+       ((forall (?order - order) 
+          (shipped ?order)))
        ())
+|#
 
      (:method (start-an-order ?order)
        ((stacks-avail ?next)
@@ -296,5 +364,9 @@
                       (not (made ?p)))
                  ?ps)
           (assign ?c (length '?ps))))
+
+     (:- (ready-to-ship ?o) ((forall ?p (includes ?o ?p) (made ?p))))
      ))
+
+
   
