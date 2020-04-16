0dbdd8fc3e17f4f3137fc329ae257be30b67525d
Robert P. Goldman <rpgoldman@sift.net>
2020-04-16 15:38:08 -0500

    Remove redundant package nickname.
    Lispworks errored out on a DEFPACKAGE form with a nickname that is
    the same as the package's proper name. This seemed stupid anyway,
    so I have removed it.

--- shop3/minimal-subtree/package.lisp
+++ shop3/minimal-subtree/package.lisp
@@ -1,7 +1,7 @@
 (in-package :common-lisp-user)
 
 (defpackage shop3-minimal-subtree
-  (:nicknames #:shop3-minimal-subtree #:subtree #:shop2-minimal-subtree)
+  (:nicknames #:shop-minimal-subtree #:subtree #:shop2-minimal-subtree)
   (:shadowing-import-from #:plan-tree #:tree-node-task #:tree-node)
   (:export #:find-failed-task)
   (:use common-lisp iterate shop2 plan-tree))
