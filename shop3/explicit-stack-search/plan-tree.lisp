2d447b4a8a488917c91ca43db879bb435799bb32
Robert P. Goldman <rpgoldman@goldman-tribe.org>
2021-11-19 14:29:05 -0600

    PLAN-TREE-COPY for enhanced plan tree.
    This is complicated substantially by the fact that the tree is indexed by checking tasks using object equality.

--- shop3/explicit-stack-search/plan-tree.lisp
+++ shop3/explicit-stack-search/plan-tree.lisp
@@ -26,6 +26,8 @@
            #:find-plan-step
            #:find-task-in-tree
            #:find-tree-node-if
+
+           #:copy-plan-tree
            ))
 
 (in-package :plan-tree)
@@ -120,7 +122,7 @@ Particularly useful for structures, but could be generally applicable."
   (print-unreadably (d str :type t)
     (format str "Unordered CHILDREN: ~A"
             (complex-tree-node-children d))))
-           
+
 (defun find-plan-step (task plan-tree &optional plan-tree-hash)
   (labels ((tree-search (plan-tree)
              (etypecase plan-tree
@@ -175,3 +177,78 @@ Particularly useful for structures, but could be generally applicable."
                       (as result = (tree-search tree-node))
                       (when result (return-from find-tree-node-if result))))))))
     (tree-search plan-tree)))
+
+(defgeneric copy-plan-tree-node (node)
+  (:method ((node primitive-tree-node))
+    (copy-primitive-tree-node node))
+  (:method ((node top-node))
+    (copy-top-node node))
+  (:method ((node ordered-tree-node))
+    (copy-ordered-tree-node node))
+  (:method ((node unordered-tree-node))
+    (copy-unordered-tree-node node))
+  (:method ((node complex-tree-node))
+    (copy-complex-tree-node node)))
+
+
+(declaim
+ (ftype
+  (function (top-node hash-table hash-table)
+            (values top-node hash-table &optional))
+  copy-plan-tree))
+
+(defun copy-plan-tree (plan-tree lookup-table translation-table)
+  "Make a new copy of PLAN-TREE (indexed by LOOKUP-TABLE), and using the
+input TRANSLATION-TABLE, which translates old primitive tasks to new primitive
+tasks."
+  (let ((tree-translation-table (make-hash-table :test #'eq))
+        (new-lookup-table (make-hash-table :test #'eq :size (hash-table-size lookup-table)))
+        new-root)
+    (flet ((translate-node (node)
+             (or (gethash node tree-translation-table)
+                 (error "Tree translation table has no translation for node ~s" node))))
+      (labels ((phase-one (node)
+                 ;; make a new node and index the copy against the original node
+                 (let ((new-node (copy-plan-tree-node node)))
+                   (setf (gethash node tree-translation-table) new-node)
+                   (when (typep new-node 'top-node)
+                     (assert (null new-root))
+                     (setf new-root new-node))
+                   ;; no recursion for primitive nodes...
+                   (if (typep node 'primitive-tree-node)
+                       (setf (tree-node-task new-node)
+                             (or (gethash (tree-node-task node) translation-table)
+                                 (error "Translation table has no translation for primitive task ~s" (tree-node-task node))))
+                       (progn
+                         (setf (tree-node-task new-node) (copy-tree (tree-node-task node)))
+                         (dolist (c (complex-tree-node-children node))
+                           (phase-one c))))))
+               ;; FIXME: need to rewrite the dependencies, too
+               (phase-two (node)
+                 ;; make a new node and index the copy against the original node
+                 (let ((new-node (gethash node tree-translation-table)))
+                   ;; copy the dependencies
+                   (setf (tree-node-dependencies new-node)
+                         (mapcar #'translate-node (tree-node-dependencies node)))
+
+                   ;; no recursion needed for primitive nodes...
+                   (unless (typep node 'primitive-tree-node)
+                     ;; otherwise copy the children
+                     (setf (complex-tree-node-children new-node)
+                           (mapcar #'translate-node (complex-tree-node-children node)))
+                     ;; and then update all the children
+                     (dolist (c (complex-tree-node-children node))
+                       (phase-one c))))))
+
+        (phase-one plan-tree)
+        (phase-two plan-tree)
+        (iter (for (task node) in-hashtable lookup-table)
+          (as indexing-task =
+              (if (primitive-tree-node-p node)
+                  (gethash task translation-table)
+                  task))
+          (setf (gethash indexing-task new-lookup-table)
+                (translate-node node)))
+        (assert new-root)
+        (setf (top-node-lookup-table new-root) new-lookup-table)
+        (values new-root new-lookup-table)))))
--- shop3/minimal-subtree/package.lisp
+++ shop3/minimal-subtree/package.lisp
@@ -2,6 +2,6 @@
 
 (defpackage shop3-minimal-subtree
   (:nicknames #:shop-minimal-subtree #:subtree #:shop2-minimal-subtree)
-  (:shadowing-import-from #:plan-tree #:tree-node-task #:tree-node)
+  (:shadowing-import-from #:plan-tree #:tree-node-task #:tree-node #:copy-plan-tree)
   (:export #:find-failed-task)
   (:use common-lisp iterate shop2 plan-tree))
