8a20b6798cd02f3d4acbbb575bd7c6322401fa19
Robert P. Goldman <rpgoldman@sift.net>
2023-07-25 11:38:17 -0500

    Partial HDDL export support.

---
+++ shop3/hddl/hddl-plan.lisp
@@ -0,0 +1,33 @@
+(in-package :hddl-translator)
+
+(defun hddl-plan (plan tree)
+  (multiple-value-bind (indexed-plan task-indices)
+      (index-shop-plan (shop:remove-costs plan))
+    (let ((next-index (1+ (caar (last indexed-plan))))
+          (root-tasks (forest-roots tree))
+          roots decompositions)
+      (setf roots
+       (iter (for root in root-tasks)
+         (setf (gethash root task-indices) next-index)
+         (collecting next-index)
+         (incf next-index)))
+
+      `(:hddl-plan
+        :actions ,indexed-plan
+        :roots ,roots
+        :decompositions ,decompositions
+        ))))
+
+(declaim (ftype (function (list) (values list hash-table &optional))
+                index-shop-plan))
+(defun index-shop-plan (action-list)
+  (let ((hash-table (make-hash-table :test 'eq))
+        (assoc-table (pairlis
+                      (alexandria:iota (length action-list))
+                      action-list)))
+    (iter (for (i . act) in assoc-table)
+      (setf (gethash act hash-table) i))
+    (values assoc-table hash-table)))
+
+(defun forest-roots (plan-tree)
+  (mapcar #'shop:tree-node-task plan-tree))
