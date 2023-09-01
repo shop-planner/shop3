3ad2bcb40cf620a4784418463befccf4dd856ba7
Robert P. Goldman <rpgoldman@sift.net>
2023-09-01 18:22:08 -0500

    Add shop3/hddl system.
    Translate SHOP plans into HDDL plans in IPC format.  Translate HDDL
    problems into SHOP problems.

--- shop3/hddl/hddl-plan.lisp
+++ shop3/hddl/hddl-plan.lisp
@@ -1,16 +1,18 @@
 (in-package :hddl-translator)
 
 (defun hddl-plan (plan tree)
+  "Take a SHOP PLAN and TREE (really a forest) as input and produce an
+HDDL plan encoded as an s-expression."
   (multiple-value-bind (indexed-plan task-indices)
       (index-shop-plan (shop:remove-costs plan))
     (let ((next-index (1+ (caar (last indexed-plan))))
           (root-tasks (forest-roots tree))
           roots decompositions)
       (setf roots
-       (iter (for root in root-tasks)
-         (setf (gethash root task-indices) next-index)
-         (collecting next-index)
-         (incf next-index)))
+            (iter (for root in root-tasks)
+              (setf (gethash root task-indices) next-index)
+              (collecting next-index)
+              (incf next-index)))
 
       `(:hddl-plan
         :actions ,indexed-plan
