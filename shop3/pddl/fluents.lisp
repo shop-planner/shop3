f1bcab8c56e367524ad1869bcfc756356fb61dbd
rpgoldman <rpgoldman@sift.net>
2023-10-26 18:10:31 -0500

    Fix package bug breaking buildapp build. (#154)
    * Fix package bug breaking buildapp build.
    
    * One additional symbol I missed.

--- shop3/pddl/fluents.lisp
+++ shop3/pddl/fluents.lisp
@@ -1,21 +1,21 @@
 ;;;
 ;;; Version: MPL 1.1/LGPL 2.1
-;;; 
+;;;
 ;;; The contents of this file are subject to the Mozilla Public License
 ;;; Version 1.1 (the "License"); you may not use this file except in
 ;;; compliance with the License. You may obtain a copy of the License at
 ;;; http://www.mozilla.org/MPL/
-;;; 
+;;;
 ;;; Software distributed under the License is distributed on an "AS IS"
 ;;; basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
 ;;; License for the specific language governing rights and limitations under
 ;;; the License.
-;;; 
+;;;
 ;;; Alternatively, the contents of this file may be used under the terms of
 ;;; the GNU Lesser General Public License Version 2.1 or later (the
 ;;; "LGPL"), in which case the provisions of the LGPL are
-;;; applicable instead of those above. 
-;;; 
+;;; applicable instead of those above.
+;;;
 ;;; Copyright (C) 2020, Robert P. Goldman and SIFT, LLC.
 
 ;;;---------------------------------------------------------------------------
@@ -24,8 +24,21 @@
 ;;; import them into the SHOP package proper.
 ;;;---------------------------------------------------------------------------
 
+(defpackage :shop.pddl-fluents
+  (:nicknames #:shop3.pddl-fluents)
+  (:use common-lisp)
+  (:import-from #:shop.theorem-prover
+                #:fluent-functions
+                #:fluents-mixin
+                #:+numerical-comparisons+
+                #:+binary-numerical-operators+
+                #:fluent-value
+                #:fluent-function-p
+                #:fluent-expr-p
+                #:fluent-comparison-p
+                #:f-exp-value))
 
-(in-package :shop3.theorem-prover)
+(in-package :shop3.pddl-fluents)
 
 (defparameter +numerical-comparisons+
   '(< > <= >= =)
