f3aa3ae7fdcc1727d719437ea05fce8173cb32f4
Robert P. Goldman <rpgoldman@sift.net>
2026-04-21 17:27:54 -0500

    Add TRANSLATE-PDDL-PROBLEM utility.

--- shop3/package.lisp
+++ shop3/package.lisp
@@ -317,6 +317,8 @@
              ;; check a plan from a PDDL domain/problem for correctness
              #:validate-plan
              #:source-pddl-domain       ; domain accessor -- helper for validation...
+             ;; PDDL translation utility
+             #:translate-pddl-problem
 
              ;; exporting for val
              #:validator-export
